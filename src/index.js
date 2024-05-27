0bda41a57aeaeb839da58feac836a0faf84c4c9d
vaiperlexmar <vaiperlexmar@gmail.com>
2024-05-27 20:39:34 +0300

    feat: You can add new tasks

--- src/index.js
+++ src/index.js
@@ -1,44 +1,15 @@
 "use strict";
 import { db, getTasks } from "./db";
-import createTask from "./Task";
+import createTask from "./createTask";
+import distributeTask from "./distributeTask";
 
 // Get tasks from DB and setting to task lists
-const overdueTasks = document.querySelector(".sublist_overdue");
-const todayTasks = document.querySelector(".sublist_today");
-const tomorrowTasks = document.querySelector(".sublist_tomorrow");
-const futureTasks = document.querySelector(".sublist_future");
-
-const todayStartDay = new Date();
-todayStartDay.setHours(0);
-todayStartDay.setMinutes(0);
-
-const todayFinishDay = new Date();
-todayFinishDay.setHours(23);
-todayFinishDay.setMinutes(59);
 
 getTasks(db).then((tasks) => {
   tasks.map((task) => {
     const taskTimestamp = task.date.seconds * 1000;
-    const startTodayTimestamp = todayStartDay.getTime();
-    const finishTodayTimestamp = todayFinishDay.getTime();
+    const newTask = createTask(task.text, taskTimestamp, task.isComplete);
 
-    let newTask = createTask(task.text, taskTimestamp, task.isComplete);
-
-    // for today
-    if (
-      taskTimestamp >= startTodayTimestamp &&
-      taskTimestamp <= finishTodayTimestamp
-    ) {
-      todayTasks.appendChild(newTask);
-    } else if (taskTimestamp < startTodayTimestamp) {
-      overdueTasks.appendChild(newTask);
-    } else if (
-      taskTimestamp > finishTodayTimestamp &&
-      taskTimestamp - finishTodayTimestamp < 86400000
-    ) {
-      tomorrowTasks.appendChild(newTask);
-    } else {
-      futureTasks.appendChild(newTask);
-    }
+    distributeTask(newTask, taskTimestamp);
   });
 });
--- src/modal.js
+++ src/modal.js
@@ -1,21 +1,26 @@
 "use strict";
 
+import {db} from "./db";
+import {collection, addDoc} from "firebase/firestore/lite";
 import flatpickr from "flatpickr";
+
+import distributeTask from "./distributeTask";
+import createTask from "./createTask";
 
 const addBtn = document.querySelector(".task-list__add-btn");
 const modal = document.querySelector(".modal");
 const container = document.querySelector(".container"); // container for blur content with modal
 
 const modalTaskInput = document.querySelector(".modal__task-name");
-const modalDatePicker = document.querySelector(".modal__datepicker");
-const modalTimePicker = document.querySelector(".modal__timepicker");
+const datePickerEl = document.querySelector(".modal__datepicker");
+const timePickerEl = document.querySelector(".modal__timepicker");
 
-flatpickr(modalDatePicker, {});
-flatpickr(modalTimePicker, {
-  enableTime: true,
-  enableSeconds: false,
-  noCalendar: true,
-  time_24hr: true,
+const modalDatePicker = flatpickr(datePickerEl, {});
+const modalTimePicker = flatpickr(timePickerEl, {
+    enableTime: true,
+    enableSeconds: false,
+    noCalendar: true,
+    time_24hr: true,
 });
 
 // ================================
@@ -23,30 +28,62 @@ flatpickr(modalTimePicker, {
 // ================================
 
 function closeModal() {
-  modalTaskInput.value = "";
-  flatpickr(modalDatePicker).clear();
-  flatpickr(modalTimePicker).clear();
+    modalTaskInput.value = "";
+    modalDatePicker.clear();
+    modalTimePicker.clear();
 
-  modal.classList.remove("scale-up-center");
-  modal.classList.add("scale-down-center");
-  setTimeout(() => {
-    modal.style.display = "none";
-    container.removeEventListener("click", closeModal);
-  }, 400);
-  container.classList.remove("blur-filter");
+    modal.classList.remove("scale-up-center");
+    modal.classList.add("scale-down-center");
+    setTimeout(() => {
+        modal.style.display = "none";
+        container.removeEventListener("click", closeModal);
+    }, 400);
+    container.classList.remove("blur-filter");
 }
 
 const modalCloseBtn = document.querySelector(".modal__close");
 modalCloseBtn.addEventListener("click", closeModal);
 
 addBtn.addEventListener("click", () => {
-  modal.classList.remove("scale-down-center");
-  modal.style.display = "block";
+    modal.classList.remove("scale-down-center");
+    modal.style.display = "block";
 
-  modal.classList.add("scale-up-center");
+    modal.classList.add("scale-up-center");
 
-  container.classList.add("blur-filter");
-  setTimeout(() => {
-    container.addEventListener("click", closeModal);
-  }, 500);
+    container.classList.add("blur-filter");
+    setTimeout(() => {
+        container.addEventListener("click", closeModal);
+    }, 500);
 });
+
+// =====================================
+//      Logic of adding new task
+// =====================================
+
+const submitBtn = document.querySelector(".modal__submit");
+
+async function addNewTask(event) {
+    event.preventDefault();
+    const timeStamp = new Date(
+        datePickerEl.value + " " + timePickerEl.value
+    ).getTime();
+
+
+    // собираем данные с инпутов в объект
+
+    // создаем таску и добавляем в список distributeTask
+    const newTaskEl = createTask(modalTaskInput.value, timeStamp, false);
+    distributeTask(newTaskEl, timeStamp);
+
+    // передаем в БД
+
+    await addDoc(collection(db, "tasks"), {
+        text: modalTaskInput.value,
+        date: new Date(timeStamp),
+        isComplete: false,
+    });
+
+    closeModal();
+}
+
+submitBtn.addEventListener("click", addNewTask);
